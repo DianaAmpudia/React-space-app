8e9176909a34c4c8dd70560cbbce8a907dd895a7
Diana Ampudia <dianalauraampudia@gmail.com>
2024-08-14 15:01:08 -0600

    add MUI app bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
-import Navbar from "./components/navbar/Navbar";
+// import Navbar from "./components/navbar/Navbar";
+import ResponsiveAppBar from "./components/appBar/Appbar.jsx"
 import Astros from "./components/astros/Astros.jsx";
 import Apod from "./components/apod/Apod.jsx";
 import AboutMePage from "./components/AboutMe.jsx";
+import {  Route, Routes } from 'react-router-dom';
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+
+
 
 function App() {
-  const links = [
-    { id: 1, href: "/", text: "Home" },
-    { id: 3, href: "/about", text: "About me" },
-    { id: 4, href: "/astros", text: "Astros" },
-    { id: 5, href: "/apod", text: "APOD" },
-  ];
-
   return (
-    <>
-      <Navbar links={links} />
+    <div>
+      <ResponsiveAppBar />
       <Routes>
         <Route path="/" element={<h1>Welcome to Space App</h1>} />
         <Route path="/about" element={<AboutMePage />} />
         <Route path="/astros" element={<Astros />} />
-        <Route path="/Apod" element={<Apod />} />
+        <Route path="/apod" element={<Apod />} />
       </Routes>
-    </>
+      </div>
   );
 }
 
