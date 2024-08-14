3a3186134e632eae7f010d094c01fa09d3e11d38
Diana Ampudia <dianalauraampudia@gmail.com>
2024-08-14 13:36:42 -0600

    add social media links to about me card

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,15 @@
 import AboutCard from "./card/AboutCard";
 import './card/AboutCard.css'
-// import {LinkedInIcon, GitHubIcon} from '@mui/icons-material/LinkedIn';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 
 const AboutMePage = () => {  
+
+  const socialMediaLinks= [
+    { url: 'https://www.linkedin.com/in/dianaampudia/', icon: <LinkedInIcon /> },
+    { url: 'https://github.com/DianaAmpudia', icon: <GitHubIcon /> },
+  ];
     return (
       <div className="page-container">
       <div className="card-container">
@@ -12,7 +18,7 @@ const AboutMePage = () => {
           title="Web Developer"
           description="I'm Diana, a junior web developer passionate about creating engaging and functional websites. I have experience with HTML, CSS, and JavaScript, and I'm eager to expand my skills in front-end and back-end technologies."
           image="/pfp.jpg"
-          
+          socialMediaLinks={socialMediaLinks}
         />
       </div>
       </div>
